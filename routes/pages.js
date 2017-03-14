bbf76beeec9d36eeff6a66f989ab3c56c78621a6
Gordon Evans <gjrevans@gmail.com>
2017-03-13 22:00:33 -0400

    cuz ayla

--- routes/pages.js
+++ routes/pages.js
@@ -2,7 +2,7 @@ var PageRoutes = function(thing){};
 
 PageRoutes.prototype.index = function(req, res) {
     res.render('index.html', {
-        page: { title: 'iSearchUFind' } ,
+        page: { title: 'Search-N-Find' } ,
         base_url: process.env.BASE_URL
     });
 }
